4ddda819e5bfe3f3e0b57aee4a858fcf3c010719
chuanyang <2214627482@qq.com>
2022-04-07 18:15:09 +0800

    Site updated: 2022-04-07 18:15:06

--- js/plugins/taohua.js
+++ js/plugins/taohua.js
@@ -105,7 +105,7 @@ function startSakura() {
     staticx = true;
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
-    canvas.setAttribute('style', 'position: fixed;left: 0;top: 0;pointer-events: none;');
+    canvas.setAttribute('style', 'position: fixed;left: 0;top: 0;pointer-events: none;z-index:1;');
     canvas.setAttribute('id', 'canvas_sakura');
     document.getElementsByTagName('body')[0].appendChild(canvas);
     cxt = canvas.getContext('2d');
